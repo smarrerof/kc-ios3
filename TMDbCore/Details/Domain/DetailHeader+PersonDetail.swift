7bebc5a746b7d788490f00292899a3fc554c12ec
Sergio Marrero <smarrerof@gmail.com>
2017-11-03 09:31:46 +0000

    Added backdropPath to person detail

--- TMDbCore/Details/Domain/DetailHeader+PersonDetail.swift
+++ TMDbCore/Details/Domain/DetailHeader+PersonDetail.swift
@@ -12,8 +12,22 @@ extension DetailHeader {
     init(person: PersonDetail, dateFormatter: DateFormatter) {
         title = person.name
         posterPath = person.profilePath
-        backdropPath = nil
-
+        
+        if let taggedImages = person.taggedImages {
+            if taggedImages.results.count > 0 {
+                switch taggedImages.results[0].media {
+                case .movie(let movie):
+                    backdropPath = movie.backdropPath
+                case .show(let show):
+                    backdropPath = show.backdropPath
+                }
+            } else {
+                backdropPath = nil
+            }
+        } else {
+            backdropPath = nil
+        }
+        
         metadata = [person.birthDay, person.deathDay].flatMap { $0 }.joined(separator: " - ")
     }
 }
